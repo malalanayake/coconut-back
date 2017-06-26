f4c6bfe5029765ae8265aec61ced5c484a675d18
Dinuka Malalanayake <dinuka.malalanayake@gmail.com>
2017-06-26 18:21:58 -0500

    Add JWT authentication with Auth0

--- src/main/java/com/sysensor/coconut/config/WebSecurityConfig.java
+++ src/main/java/com/sysensor/coconut/config/WebSecurityConfig.java
@@ -1,40 +1,29 @@
 package com.sysensor.coconut.config;
 
-import com.sysensor.coconut.config.security.JWTAuthenticationFilter;
-import com.sysensor.coconut.config.security.JWTLoginFilter;
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${auth0.audience}")
+    private String audience;
+
+    @Value("${auth0.issuer}")
+    private String issuer;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.POST, URLConfig.AUTHENTICATE).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                // We filter the api/authenticate requests
-                .addFilterBefore(new JWTLoginFilter(URLConfig.AUTHENTICATE, authenticationManager()),
-                        UsernamePasswordAuthenticationFilter.class)
-                // And filter other requests to check the presence of JWT in header
-                .addFilterBefore(new JWTAuthenticationFilter(),
-                        UsernamePasswordAuthenticationFilter.class);
-    }
+                .antMatchers(URLConfig.API + "/**").authenticated();
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // Create a default account
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("password")
-                .roles("ADMIN");
+        JwtWebSecurityConfigurer
+                .forRS256(audience, issuer)
+                .configure(http);
     }
 }
